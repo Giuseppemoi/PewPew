82335af71b05c0ec2cf8f19169a3f089ed7f641c
GiuseppeMoi <giuseppemoi1989@gmail.com>
2021-07-17 23:01:13 +0200

    change timer

--- assets/script.js
+++ assets/script.js
@@ -12,69 +12,36 @@ var xval = (canvas.width/2) - 35;
 var score = 0
 var play = false;
 var start;
-var popTarget;
-var startTimer;
+// var popTarget;
+// var startTimer;
 
 var difficulty = 1;
 var theme = 0;
-var timer = 0;
 var minutes = 0;
 var numberTarget = 5;
 var live = 3;
 
-function timerScore() {
-    timer++;
-}
+// function timerScore() {
+//     timer++;
+// }
 
 document.getElementById("buttonStart").addEventListener("click", () => {
-    // if(difficulty === 0) {
-    //     document.getElementById("winLose").innerHTML = "Choice your difficulty !";
-    // }
-    // else {
-    //     document.getElementById("winLose").innerHTML = "";
         if (play) {
             document.getElementById("buttonStart").innerHTML = "Start game";
             clearInterval(start);
-            clearInterval(startTimer);
+            // clearInterval(startTimer);
             play = false;
         } else {
             document.getElementById("buttonStart").innerHTML = "Pause game";
             start = setInterval(draw, 10);
-            startTimer = setInterval(timerScore, 1000)
+            // startTimer = setInterval(timerScore, 1000)
             play = true;
         }
-    //     if(play === true) {
-    //         document.getElementById("buttonDifficulty").disabled = true;
-    //     }
-    //     if(play === true) {
-    //         document.getElementById("buttonTheme").disabled = true;
-    //     }
-    // }
 });
 
 document.getElementById("buttonReset").addEventListener("click", () => {
     window.location.reload()
 })
-
-// document.getElementById("buttonDifficulty").addEventListener("click", () => {
-//     difficulty++;
-//     if(difficulty === 1) {
-//         document.getElementById("buttonDifficulty").innerHTML = "Difficulty: Easy";
-//     }
-//     else if(difficulty === 2) {
-//         document.getElementById("buttonDifficulty").innerHTML = "Difficulty: Medium";
-//     }
-//     else if(difficulty === 3) {
-//         document.getElementById("buttonDifficulty").innerHTML = "Difficulty: Hard";
-//     }
-//     else if(difficulty === 4) {
-//         document.getElementById("buttonDifficulty").innerHTML = "Difficulty: Extrem";
-//     }
-//     else if(difficulty === 5) {
-//         difficulty = 1;
-//         document.getElementById("buttonDifficulty").innerHTML = "Difficulty: Easy";
-//     }
-// });
 
 document.getElementById("buttonTheme").addEventListener("click", () => {
     theme++;
@@ -104,10 +71,8 @@ var dy = -2;
 
 var shipX;
 document.addEventListener('mousemove', function(event) {
-    
     if (event.path[0].id === "game") {
         shipX = event.clientX-35
-        
     }
 });
 
@@ -121,16 +86,15 @@ for (let i = 0; i < maxHaert; i++) {
 function drawHaert() {
     for (let i = 0; i < arrayHaert.length; i++) {
         ctx.drawImage(imgHaert, arrayHaert[i].x, arrayHaert[i].y, imgHaert.width = 15, imgHaert.height = 15);
-        arrayHaert[i].y += 1 * difficulty; 
+        arrayHaert[i].y += 2 * difficulty; 
     }
 }
+
 function newHaert(){
     if (haert == arrayHaert.length) {
-        
         haert = 0
         arrayHaert = [];
         for (let i = 0; i < maxHaert; i++) {
-            
             arrayHaert.push({x: Math.floor(Math.random() * (canvas.width -35)), y: Math.floor(Math.random() * -500)-35})
         }
     }
@@ -165,7 +129,6 @@ function drawShip (){
 
 var arrayProjectile = [];
 document.addEventListener("mousedown", function setProjectile(event) {
-    
     if (event.path[0].id === "game") {
         arrayProjectile.push([shipX, y])        
     }
@@ -174,11 +137,9 @@ document.addEventListener("mousedown", function setProjectile(event) {
 var numTarget;
 function drawProjectile() {
     for(numTarget = 0; numTarget < arrayProjectile.length; numTarget++){
-        
         for (let i = arrayTarget.length-1; i > -1; i--) {
             if (arrayProjectile[numTarget] != undefined) {
                 if(arrayTarget[i].x < arrayProjectile[numTarget][0]+35 + 5 && arrayTarget[i].x + 35 > arrayProjectile[numTarget][0]+35 && arrayTarget[i].y < arrayProjectile[numTarget][1] + 5 && 35 + arrayTarget[i].y > arrayProjectile[numTarget][1]) { //shoot hit
-                    
                     ctx.clearRect(arrayTarget[i].x, arrayTarget[i].y, image2.width = 35, image2.height = 35);
                     arrayProjectile.splice(numTarget, 1);
                     arrayTarget.splice(i, 1, {});
@@ -202,8 +163,8 @@ function drawProjectile() {
         }
     }
 }
-var liveFlex = document.getElementById("liveFlex")
 
+var liveFlex = document.getElementById("liveFlex")
 function collides() {
     for (let i = 0; i < arrayTarget.length; i++) {
         if(arrayTarget[i].x < shipX + 70 && arrayTarget[i].x + 35 > shipX && arrayTarget[i].y < yval + 70 && 35 + arrayTarget[i].y > yval) { //target hit
@@ -215,28 +176,11 @@ function collides() {
             live--;
             checkLive();
         }
-        // if(arrayHaert[i].x < shipX + 70 && arrayHaert[i].x + 15 > shipX && arrayHaert[i].y < yval + 70 && 15 + arrayHaert[i].y > yval) { //target hit
-        //     ctx.clearRect(arrayTarget[i].x, arrayTarget[i].y, image2.width = 35, image2.height = 35);
-        //     arrayHaert.splice(i, 1, {});
-        //     live++;
-        //     haert++;
-        //     liveFlex.innerHTML += `<img id="live${live}" class="heartClass" src="assets/img/hearticon-removebg-preview.png"></img>`;
-        //     
-        //     
-        // }
         if(arrayTarget[i].y > canvas.height) {
             ctx.clearRect(arrayTarget[i].x, arrayTarget[i].y, image2.width = 35, image2.height = 35);
             arrayTarget.splice(i, 1, {});
             target++;
         }
-        // if(arrayHaert[i].y > canvas.height) {
-        //     ctx.clearRect(arrayHaert[i].x, arrayHaert[i].y, imgHaert.width = 35, imgHaert.height = 35);
-        //     arrayHaert.splice(i, 1, {});
-        //     haert++;
-        //     
-        //     
-        // }
-        
     }
 }
 
@@ -248,31 +192,20 @@ function collidesHaert(){
             live++;
             haert++;
             liveFlex.innerHTML += `<img id="live${live}" class="heartClass" src="assets/img/hearticon-removebg-preview.png"></img>`;
-            
-            
         }
         if(arrayHaert[i].y > canvas.height) {
             ctx.clearRect(arrayHaert[i].x, arrayHaert[i].y, imgHaert.width = 35, imgHaert.height = 35);
             arrayHaert.splice(i, 1, {});
             haert++;
-            
-            
         }
     }
 }
 
 function checkLive() {
-    /*if(live === 2 ){
-    document.getElementById("live3").setAttribute("style", "display: none");
-    }
-    else if(live === 1 ){
-        document.getElementById("live2").setAttribute("style", "display: none");
-    }
-    else*/ if(live === 0) {
+    if(live === 0) {
         clearInterval(start);
         document.getElementById("winLose").innerHTML = "You Lose !";
         document.getElementById("game").setAttribute("style", "opacity: 0.5");
-        //document.getElementById("live").setAttribute("style", "display: none");
     }
 }
 
@@ -280,27 +213,40 @@ function logScore() {
     document.getElementById("score").innerHTML = score;
 }
 
-// function win() {
-//     if (score === numberTarget) {
-//         clearInterval(inter);
-//         clearInterval(interTimer);
-//         document.getElementById("winLose").innerHTML = "You win !";
-//         document.getElementById("game").setAttribute("style", "opacity: 0.5");
-//     }
-// }
+var timer = 0;
 function time(){
-    if(timer < 10) {
-        document.getElementById("gameTime").innerHTML = "0" + minutes + " : " + 0 + timer;
+    timer++
+    if(timer < 999) {
+        document.getElementById("gameTime").innerHTML = "00" + " : " + "0" + Math.floor(timer/100);
+    } else if (timer > 999 && timer < 5999){
+        document.getElementById("gameTime").innerHTML = "00" + " : " + Math.floor(timer/100);
+    } else if (timer > 5999 && timer < 59999){
+        if ((timer/100)%60 < 10) {
+            document.getElementById("gameTime").innerHTML = "0" + Math.floor(timer/6000) + " : " + "0" + Math.floor((timer/100)%60);
+        } else {
+            document.getElementById("gameTime").innerHTML = "0" + Math.floor(timer/6000) + " : " + Math.floor((timer/100)%60);
+        }
+    } else if (timer > 59999 && timer < 359999){
+        if ((timer/100)%60 < 10) {
+            document.getElementById("gameTime").innerHTML = Math.floor(timer/6000) + " : " + "0" + Math.floor((timer/100)%60);
+        } else {
+            document.getElementById("gameTime").innerHTML = Math.floor(timer/6000) + " : " + Math.floor((timer/100)%60);
+        }
+    } else if (timer > 359999){
+        if ((timer/100)%60 < 10) {
+            if ((timer/100)%6000 < 10) {
+                document.getElementById("gameTime").innerHTML = "0" + Math.floor(timer/360000) + " : " + "0" + Math.floor((timer - 360000) / 6000) + " : " + "0" + Math.floor((timer/100)%60);
+            } else {document.getElementById("gameTime").innerHTML = "0" + Math.floor(timer/360000) + " : " + Math.floor((timer - 360000) / 6000) + " : " + "0" + Math.floor((timer/100)%60);
+            }
+        } else {
+            if ((timer/100)%6000 > 10) {
+                document.getElementById("gameTime").innerHTML = "0" + Math.floor(timer/360000) + " : " + Math.floor((timer - 360000) / 6000) + " : " + Math.floor((timer/100)%60);
+            } else {
+                document.getElementById("gameTime").innerHTML = "0" + Math.floor(timer/360000) + " : " + "0" + Math.floor((timer - 360000) / 6000) + " : " + Math.floor((timer/100)%60);
+            }
+        }
     }
-    else {
-        document.getElementById("gameTime").innerHTML = "0" + minutes + " : " + timer;
-    }
-    if(timer === 60) {
-        timer = 0;
-        minutes += 1;
-    } 
 }
-
 
 function draw(){
     ctx.clearRect(0, 0, canvas.width, canvas.height);
@@ -308,7 +254,6 @@ function draw(){
     drawProjectile()
     drawShip();
     logScore()
-    //win()
     time()
     newTarget()
     drawHaert()
@@ -316,5 +261,3 @@ function draw(){
     collides()
     collidesHaert()
 }
-
-
