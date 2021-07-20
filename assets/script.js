80d7785eac40dff93e0a6048d08bd05a4c3ffb11
GiuseppeMoi <giuseppemoi1989@gmail.com>
2021-07-20 23:34:03 +0200

    add bounce control

--- assets/script.js
+++ assets/script.js
@@ -18,6 +18,9 @@ var theme = 0;
 var numberTarget = 5;
 var live = 3;
 
+var animControl = document.getElementById("bounce");
+animControl.style.animation = "bounce 0.7s ease infinite";
+
 window.addEventListener("resize", (event) => {
     if (event) {
         canvas.width  = window.innerWidth;
@@ -59,16 +62,22 @@ document.getElementById("buttonReset").addEventListener("click", () => {
 document.getElementById("buttonTheme").addEventListener("click", () => {
     theme++
     if(theme == 1) {
+        animControl.style.animation = "";
         document.querySelector("body").removeAttribute("class");
         document.querySelector("body").setAttribute("class", "purple");
     } else if(theme === 2) {
+        animControl.style.animation = "";
         document.querySelector("body").removeAttribute("class");
         document.querySelector("body").setAttribute("class", "jupiter");
     } else if(theme === 3) {
+        animControl.style.animation = "";
         document.querySelector("body").removeAttribute("class");
         document.querySelector("body").setAttribute("class", "milkyWay");
     } else if(theme === 4) {
+        animControl.style.animation = "";
         theme = 1;
+        animControl.removeAttribute("animation");
+        animControl.removeAttribute("-webkit-animation");
         document.querySelector("body").removeAttribute("class");
         document.querySelector("body").setAttribute("class", "purple");
     }
